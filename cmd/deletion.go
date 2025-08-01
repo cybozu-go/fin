5a735359687c65eee5cbf65c45f05b7cb007f82c
Kyori Sakao <sakaok@cybozu.co.jp>
2025-08-01 08:10:25 +0000

    deletion job: add entry point
    Signed-off-by: Kyori Sakao <sakaok@cybozu.co.jp>

---
+++ cmd/deletion.go
@@ -0,0 +1,93 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/cybozu-go/fin/internal/infrastructure/ceph"
+	"github.com/cybozu-go/fin/internal/infrastructure/db"
+	"github.com/cybozu-go/fin/internal/infrastructure/nlv"
+	"github.com/cybozu-go/fin/internal/job"
+	"github.com/cybozu-go/fin/internal/job/deletion"
+	"github.com/cybozu-go/fin/internal/model"
+	"github.com/spf13/cobra"
+)
+
+var deletionCmd = &cobra.Command{
+	Use: "deletion",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return deletionJobMain()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deletionCmd)
+}
+
+func deletionJobMain() error {
+	pvcName := os.Getenv("BACKUP_TARGET_PVC_NAME")
+	if pvcName == "" {
+		return fmt.Errorf("BACKUP_TARGET_PVC_NAME environment variable is not set")
+	}
+	pvcNamespace := os.Getenv("BACKUP_TARGET_PVC_NAMESPACE")
+	if pvcNamespace == "" {
+		return fmt.Errorf("BACKUP_TARGET_PVC_NAMESPACE environment variable is not set")
+	}
+	pvcUID := os.Getenv("BACKUP_TARGET_PVC_UID")
+	if pvcUID == "" {
+		return fmt.Errorf("BACKUP_TARGET_PVC_UID environment variable is not set")
+	}
+
+	rootPath := filepath.Join(nlv.VolumePath, pvcNamespace, pvcName)
+	nlvRepo, err := nlv.NewNodeLocalVolumeRepository(rootPath)
+	if err != nil {
+		return fmt.Errorf("failed to create NodeLocalVolumeRepository: %w", err)
+	}
+	defer func() { _ = nlvRepo.Close() }()
+
+	finRepo, err := db.New(nlvRepo.GetDBPath())
+	if err != nil {
+		return fmt.Errorf("failed to create repository: %w", err)
+	}
+	defer func() { _ = finRepo.Close() }()
+	rbdRepo := ceph.NewRBDRepository()
+
+	actionUID := os.Getenv("ACTION_UID")
+	if actionUID == "" {
+		return fmt.Errorf("ACTION_UID environment variable is not set")
+	}
+	targetSnapshotIDStr := os.Getenv("TARGET_SNAPSHOT_ID")
+	if targetSnapshotIDStr == "" {
+		return fmt.Errorf("TARGET_SNAPSHOT_ID environment variable is not set")
+	}
+	targetSnapshotID, err := strconv.Atoi(targetSnapshotIDStr)
+	if err != nil {
+		return fmt.Errorf("invalid TARGET_SNAPSHOT_ID: %w", err)
+	}
+
+	d := deletion.NewDeletion(&deletion.DeletionInput{
+		Repo:                finRepo,
+		RBDRepo:             rbdRepo,
+		NodeLocalVolumeRepo: nlvRepo,
+		ActionUID:           actionUID,
+		TargetSnapshotID:    targetSnapshotID,
+		TargetPVCUID:        pvcUID,
+	})
+	for {
+		err = d.Perform()
+		if err == nil {
+			slog.Info("Deletion job completed successfully")
+			return nil
+		}
+		if !errors.Is(err, model.ErrBusy) {
+			return fmt.Errorf("failed to perform deletion: %w", err)
+		}
+		slog.Warn("failed to acquire lock, will retry.")
+		time.Sleep(job.RetryInterval)
+	}
+}
