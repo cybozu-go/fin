766ed0452027f425849c1942a86c30f6fcfee1a4
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2025-08-05 11:33:29 +0000

    test: update cleanup job tests
    - Run tests in the same package as the test target
    - Check the existence of pvc.yaml and pv.yaml on cleanup
      ongoing full backup.
    - Enrich comments
    
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- internal/job/cleanup/cleanup_test.go
+++ internal/job/cleanup/cleanup_test.go
@@ -1,14 +1,12 @@
-package cleanup_test
+package cleanup
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/cybozu-go/fin/internal/job"
-	"github.com/cybozu-go/fin/internal/job/cleanup"
 	"github.com/cybozu-go/fin/internal/job/input"
 	"github.com/cybozu-go/fin/internal/job/testutil"
 	"github.com/cybozu-go/fin/internal/model"
@@ -17,13 +15,40 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func putFakePVCAndPV(t *testing.T, root string) {
+	t.Helper()
+
+	pvc, err := os.Create(filepath.Join(root, "pvc.yaml"))
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		_ = pvc.Close()
+		_ = os.Remove(filepath.Join(root, "pvc.yaml"))
+	})
+	pv, err := os.Create(filepath.Join(root, "pv.yaml"))
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		_ = pv.Close()
+		_ = os.Remove(filepath.Join(root, "pv.yaml"))
+	})
+}
+
+func assertAllFullBackupDataDoNotExist(t *testing.T, root string) {
+	t.Helper()
+
+	assert.NoFileExists(t, filepath.Join(root, "pvc.yaml"))
+	assert.NoFileExists(t, filepath.Join(root, "pv.yaml"))
+	assert.NoFileExists(t, filepath.Join(root, "raw.img"))
+}
+
 func TestCleanup_OnStoringDiffForFullBackup_Success(t *testing.T) {
+	// CSATEST-1567
 	// Description:
 	//   Emulate the cleanup on storing diff for full backup.
 	//
 	// Arrange:
 	//   1. Start a backup action.
-	//   2. Create the diff directory for full backup.
+	//   2. Create pvc.yaml and pv.yaml
+	//   3. Create the diff directory for full backup.
 	//
 	// Act:
 	//   Run the cleanup process.
@@ -31,6 +56,7 @@ func TestCleanup_OnStoringDiffForFullBackup_Success(t *testing.T) {
 	// Assert:
 	//   - No in-progress action.
 	//   - Diff directory doesn't exist.
+	//   - pvc.yaml and pv.yaml are removed.
 
 	// Arrange
 
@@ -43,6 +69,7 @@ func TestCleanup_OnStoringDiffForFullBackup_Success(t *testing.T) {
 	snapID := 1
 	err = nlvRepo.MakeDiffDir(snapID)
 	require.NoError(t, err)
+	putFakePVCAndPV(t, root)
 
 	input := input.Cleanup{
 		Repo:                finRepo,
@@ -53,7 +80,7 @@ func TestCleanup_OnStoringDiffForFullBackup_Success(t *testing.T) {
 	}
 
 	// Act
-	cleanupJob := cleanup.NewCleanup(&input)
+	cleanupJob := NewCleanup(&input)
 	err = cleanupJob.Perform()
 	require.NoError(t, err)
 
@@ -61,15 +88,28 @@ func TestCleanup_OnStoringDiffForFullBackup_Success(t *testing.T) {
 	_, err = job.GetBackupMetadata(finRepo)
 	assert.ErrorIs(t, err, model.ErrNotFound)
 
-	_, err = os.Stat(filepath.Join(root, fmt.Sprintf("diff/%d", snapID)))
-	assert.ErrorIs(t, err, fs.ErrNotExist)
-
+	assert.NoFileExists(t, filepath.Join(root, fmt.Sprintf("diff/%d", snapID)))
+	assertAllFullBackupDataDoNotExist(t, root)
 	anotherActionUID := uuid.New().String()
+
 	err = finRepo.StartOrRestartAction(anotherActionUID, model.Backup)
 	require.NoError(t, err)
 }
 
 func TestCleanup_ErrorBusy(t *testing.T) {
+	// CSATEST-1566
+	// Description:
+	//   Test cleanup behavior when repository is busy with another action.
+	//
+	// Arrange:
+	//   Repository is already locked by a different action UID.
+	//
+	// Act:
+	//   Run the cleanup action with a different action UID.
+	//
+	// Assert:
+	//   Check if the cleanup fails with ErrCantLock error.
+
 	// Arrange
 	actionUID := uuid.New().String()
 	differentActionUID := uuid.New().String()
@@ -80,7 +120,7 @@ func TestCleanup_ErrorBusy(t *testing.T) {
 	require.NoError(t, err)
 
 	// Act
-	backup := cleanup.NewCleanup(&input.Cleanup{
+	backup := NewCleanup(&input.Cleanup{
 		Repo:      finRepo,
 		ActionUID: actionUID,
 	})
