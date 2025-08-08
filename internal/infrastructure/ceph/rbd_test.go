3a397c92225348c1434cad8b37107f6f0328a6f2
Kohya Shiozaki <kouyan120706@gmail.com>
2025-08-08 07:27:34 +0000

    wip

--- internal/infrastructure/ceph/rbd_test.go
+++ internal/infrastructure/ceph/rbd_test.go
@@ -517,6 +517,65 @@ func TestApplyDiffToRawImage_error_OverlappedDataRecords(t *testing.T) {
 	assert.Error(t, err)
 }
 
+func TestApplyDiffToBlockDevice_success_ExistentFromSnap(t *testing.T) {
+	// Description:
+	// Success case of diff application to block device when FROM SNAP exists
+	//
+	// Arrange:
+	// - block device exists
+	// - Incremental data file exists containing UPDATED DATA, ZERO DATA and FROM SNAP
+	//
+	// Act:
+	// Call the incremental data file application process with target snapshot name
+	//
+	// Assert:
+	// All of the following conditions are met:
+	// - Process completes successfully
+	// - Block device is overwritten for length bytes from offset according to UPDATED DATA
+	// - Block device is overwritten for length bytes from offset with 0 according to ZERO DATA
+	// - Areas not included in either UPDATED DATA or ZERO DATA remain unchanged
+
+	// Arrange
+	reader, err := diffgenerator.Run(
+		diffgenerator.WithFromSnapName("fromSnap"),
+		diffgenerator.WithToSnapName("toSnap"),
+		diffgenerator.WithImageSize(30),
+		diffgenerator.WithRecords([]*diffgenerator.DataRecord{
+			diffgenerator.NewUpdatedDataRecord(0, 10, []byte("0123456789")),
+			diffgenerator.NewZeroDataRecord(10, 20),
+		}),
+	)
+	require.NoError(t, err)
+
+	blockDevicePath := getBlockDevicePathForTest(t)
+	zerooutWholeBlockDevice(t, blockDevicePath)
+
+	file := openFileWriteOnly(t, blockDevicePath)
+	_, err = io.Copy(file, bytes.NewReader(bytes.Repeat([]byte{0xff}, 35)))
+	require.NoError(t, err)
+
+	// Act
+	err = applyDiffToBlockDevice(blockDevicePath, reader, "fromSnap", "toSnap")
+
+	// Assert
+	assert.NoError(t, err)
+
+	file = openFile(t, blockDevicePath)
+	head := make([]byte, 35)
+	_, err = io.ReadFull(file, head)
+	require.NoError(t, err)
+	assert.Equal(
+		t,
+		// UPDATED DATA ("0123456789") + ZERO DATA (20 bytes) + old 0xff data (5 bytes)
+		[]byte("0123456789\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"+
+			"\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff"),
+		head,
+	)
+
+	// Ensure the rest of the block device is zeroed out
+	compareReaders(t, file, io.LimitReader(&zeroReader{}, int64(getBlockDeviceSize(t, file)-35)))
+}
+
 func TestApplyDiffToBlockDevice_success_MissingFromSnap(t *testing.T) {
 	// Description:
 	// Success case of diff application to block device when FROM SNAP does not exist
