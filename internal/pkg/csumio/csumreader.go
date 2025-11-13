3bbbbc7a8170dd9359d24f320ba89959b3f22a16
Kohya Shiozaki <kouyan120706@gmail.com>
2025-11-13 02:53:13 +0000

    pkg: add csumreader to csumio package for data corruption detection
        - add csumio package and implement csumreader
        - add simple test in csumreader_test.go
    
    Signed-off-by: Kohya Shiozaki <kouyan120706@gmail.com>

---
+++ internal/pkg/csumio/csumreader.go
@@ -0,0 +1,73 @@
+package csumio
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/cespare/xxhash/v2"
+)
+
+var (
+	ErrChecksumMismatch = errors.New("checksum mismatch")
+)
+
+type Reader struct {
+	dataReader           io.Reader
+	checksumReader       io.Reader
+	buf                  []byte
+	offset               int
+	chunkSize            int
+	enableChecksumVerify bool
+}
+
+func NewReader(dataFile, checksumFile io.Reader, chunkSize int, enableChecksumVerify bool) (*Reader, error) {
+	if enableChecksumVerify {
+		if chunkSize < 4*1024 && chunkSize%(4*1024) != 0 {
+			return nil, fmt.Errorf("chunksize must be at least 4 KiB and a multiple of 4 KiB when checksum verification is enabled")
+		}
+	}
+	return &Reader{
+		dataReader:           dataFile,
+		checksumReader:       checksumFile,
+		chunkSize:            chunkSize,
+		enableChecksumVerify: enableChecksumVerify,
+		buf:                  make([]byte, chunkSize),
+	}, nil
+}
+
+func (cr *Reader) Read(p []byte) (int, error) {
+	if cr.offset == 0 {
+		n, err := io.ReadFull(cr.dataReader, cr.buf)
+		if err == io.ErrUnexpectedEOF {
+			cr.buf = cr.buf[:n]
+		} else if err == io.EOF {
+			return 0, io.EOF
+		} else if err != nil {
+			return 0, fmt.Errorf("failed to read data block: %w", err)
+		}
+
+		if cr.enableChecksumVerify {
+			checksumBytes := make([]byte, ChecksumLen)
+			_, err := io.ReadFull(cr.checksumReader, checksumBytes)
+			if err != nil {
+				return 0, fmt.Errorf("failed to read checksum: %w", err)
+			}
+
+			expected := binary.LittleEndian.Uint64(checksumBytes)
+			actual := xxhash.Sum64(cr.buf)
+			if expected != actual {
+				return 0, fmt.Errorf("%w: expected %016x, got %016x", ErrChecksumMismatch, expected, actual)
+			}
+		}
+		// No checksum reading when verification is disabled
+	}
+
+	n := copy(p, cr.buf[cr.offset:])
+	cr.offset += n
+	if cr.offset >= len(cr.buf) {
+		cr.offset = 0
+	}
+	return n, nil
+}
