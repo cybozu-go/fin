3dc0590d0ec8e6c02e46f6533ccc75c1023a8ea9
Kyori Sakao <sakaok@cybozu.co.jp>
2025-11-14 16:24:23 +0000

    feat: add backupCreateStatus metric
    Signed-off-by: Kyori Sakao <sakaok@cybozu.co.jp>

--- internal/pkg/metrics/metrics.go
+++ internal/pkg/metrics/metrics.go
@@ -12,6 +12,9 @@ import (
 const (
 	metricNamespace = "fin"
 
+	backupKindFull        = "full"
+	backupKindIncremental = "incremental"
+
 	// Labels
 	pvcLabel        = "pvc"
 	pvcNSLabel      = "pvc_namespace"
@@ -19,7 +22,7 @@ const (
 	fbcLabel        = "finbackupconfig"
 	cephNSLabel     = "ceph_namespace"
 	nodeLabel       = "node"
-	backupTypeLabel = "backup_type"
+	backupKindLabel = "backup_create_kind"
 )
 
 var (
@@ -30,7 +33,16 @@ var (
 			Help:      "Duration of backup operations in seconds",
 			Buckets:   []float64{600, 1800, 3600, 10800, 21600, 43200, 86400, 172800}, // 10m, 30m, 1h, 3h, 6h, 12h, 24h, 48h
 		},
-		[]string{pvcLabel, pvcNSLabel, cephNSLabel, backupTypeLabel},
+		[]string{pvcLabel, pvcNSLabel, cephNSLabel},
+	)
+
+	backupCreateStatus = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: metricNamespace,
+			Name:      "backup_create_status",
+			Help:      "Current backup execution state per PVC and backup kind",
+		},
+		[]string{cephNSLabel, pvcNSLabel, pvcLabel, backupKindLabel},
 	)
 
 	registerOnce sync.Once
@@ -46,16 +58,29 @@ func SetBackupDurationSeconds(fb *finv1.FinBackup, untilCondition, cephNamespace
 		return
 	}
 	duration := end.LastTransitionTime.Sub(start.LastTransitionTime.Time)
-	backupType := "incremental"
-	if fullBackup {
-		backupType = "full"
+	backupDurationSeconds.WithLabelValues(fb.Spec.PVC, fb.Spec.PVCNamespace, cephNamespace).Observe(float64(duration.Seconds()))
+}
+
+func SetBackupCreateStatus(fb *finv1.FinBackup, cephNamespace string, inProgress, fullBackup bool) {
+	if fb == nil {
+		return
 	}
-	backupDurationSeconds.WithLabelValues(fb.Spec.PVC, fb.Spec.PVCNamespace, cephNamespace, backupType).Observe(float64(duration.Seconds()))
+	var fullValue, incrementalValue float64
+	if inProgress {
+		if fullBackup {
+			fullValue = 1.0
+		} else {
+			incrementalValue = 1.0
+		}
+	}
+	backupCreateStatus.WithLabelValues(cephNamespace, fb.Spec.PVCNamespace, fb.Spec.PVC, backupKindFull).Set(fullValue)
+	backupCreateStatus.WithLabelValues(cephNamespace, fb.Spec.PVCNamespace, fb.Spec.PVC, backupKindIncremental).Set(incrementalValue)
 }
 
 func Register() {
 	registerOnce.Do(func() {
 		metrics.Registry.MustRegister(
+			backupCreateStatus,
 			backupDurationSeconds,
 		)
 	})
